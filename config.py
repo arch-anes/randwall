1176108cc133dc1c94290ff8aaba16fb45586b89
Anes Belfodil <ans.belfodil@gmail.com>
2020-12-01 13:04:17 -0500

    Write config for the first time after reading it

--- config.py
+++ config.py
@@ -8,51 +8,52 @@ from log import get_logger
 
 logger = get_logger(__name__)
 
-default_config = {
-    'api_key': '',
-    'max_page': 500,
-    'interval': 30,
-    'categories': {
-        'general': True,
-        'anime': True,
-        'people': True,
-    },
-    'purity': {
-        'sfw': True,
-        'sketchy': False,
-        'nsfw': False,
-    },
-    'include': [],
-    'exclude': ['microsoft', 'logo'],
-}
-
-config_dir = user_config_dir(appname="randwall")
-if not path.exists(config_dir):
-    makedirs(config_dir)
-
-config_file_path = f'{config_dir}/config.json'
-if not path.exists(config_file_path):
-    logger.info(f"Creating default config at {config_file_path}")
-
-    with open(config_file_path, 'wt') as f:
-        json.dump(default_config, f, sort_keys=True, indent=2)
-
 
 class Config:
+    config_dir = user_config_dir(appname="randwall")
+    config_file_path = f'{config_dir}/config.json'
+
+    default_config = {
+        'api_key': '',
+        'max_page': 500,
+        'interval': 30,
+        'categories': {
+            'general': True,
+            'anime': True,
+            'people': True,
+        },
+        'purity': {
+            'sfw': True,
+            'sketchy': False,
+            'nsfw': False,
+        },
+        'include': [],
+        'exclude': ['microsoft', 'logo'],
+    }
+
     def __init__(self):
         self.raw_config = self._load_config()
+        self._write_config()
+
+    def _write_config(self):
+        if not path.exists(self.config_dir):
+            makedirs(self.config_dir)
+
+        logger.info(f"Writing config to {self.config_file_path}.")
+        with open(self.config_file_path, 'wt') as f:
+            json.dump(self.raw_config, f, sort_keys=True, indent=2)
 
     def _load_config(self):
-        with open(config_file_path, 'rt') as f:
-            logger.info(f"Reading config from {config_file_path}")
+        with open(self.config_file_path, 'rt') as f:
+            logger.info(f"Reading config from {self.config_file_path}")
             try:
                 loaded_config = json.load(f)
-                return {**default_config, **loaded_config}
+                return {**self.default_config, **loaded_config}
             except:
-                logger.error(f"An error occured when reading config from {config_file_path}")
+                logger.error(f"An error occured when reading config from {self.config_file_path}")
 
     def watch_config_changes(self):
-        for changes in watch(config_file_path):
+        for changes in watch(self.config_file_path):
             self.raw_config = self._load_config()
 
 
